58adbc1f66af713eeb89a39f945bcc62f4bd35ac
nukemeso <poppo0505920@yahoo.co.jp>
2016-09-28 23:46:01 +0900

    一個ずつ出るようにした

--- aminps.js
+++ aminps.js
@@ -1,10 +1,3 @@
-function kstgOP(){
-$(".kuso_tg").fadeIn("slow")
-}
-function SKOP(){
-$(".SK_tg").fadeIn("slow")
-}
-
 function tec(){
 alert("Hello World!")
 }
@@ -14,16 +7,18 @@ alert("Hello World!")
 
 jQuery(document).ready(setReady);
 function setReady() {
-jQuery('.kuso').click(se);
+jQuery('.kuso').click(plf);
 jQuery('.SK').click(sko);
 }
 	
-function se(){
+function plf(){
+$(".kuso_tg").slideToggle()
+$(".nk").not(".kuso_tg").hide("slow");
 
-$(".kuso_tg").slideToggle();
 
 }
 
 function sko(){
 $(".SK_tg").slideToggle();
+$(".nk").not(".SK_tg").hide("slow");
 }	
